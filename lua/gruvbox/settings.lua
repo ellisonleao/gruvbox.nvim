6a0ccb52a4bb1256b50399bcd977920f63fefeec
Muhammad Ahmad 🌌 <mhdahmadx@gmail.com>
2021-12-07 11:06:14 +0200

    refactor(gruvbox): remove lush dependency refactor(gruvbox): remove filetypes highlights for treesitter perf(gruvbox): reduce load time to ~5ms

--- lua/gruvbox/settings.lua
+++ lua/gruvbox/settings.lua
@@ -1,84 +1,84 @@
 -- gruvbox settings handler
-local utils = require("gruvbox.utils")
+local utils = require "gruvbox.utils"
 
 local settings = {
-  contrast_dark = "medium",
-  contrast_light = "medium",
-  bold = true,
-  italic = true,
-  undercurl = true,
-  underline = true,
-  inverse = true,
-  improved_strings = false,
-  improved_warnings = false,
-  invert_signs = false,
-  invert_selection = true,
-  invert_tabline = false,
-  italicize_comments = true,
-  italicize_strings = false,
-  invert_intend_guides = false,
+    contrast_dark = "medium",
+    contrast_light = "medium",
+    bold = true,
+    italic = true,
+    undercurl = true,
+    underline = true,
+    inverse = true,
+    improved_strings = false,
+    improved_warnings = false,
+    invert_signs = false,
+    invert_selection = true,
+    invert_tabline = false,
+    italicize_comments = true,
+    italicize_strings = false,
+    invert_intend_guides = false,
 }
 
 local styles = {
-  italic = "italic",
-  bold = "bold",
-  underline = "underline",
-  inverse = "inverse",
-  undercurl = "undercurl",
-  invert_signs = "",
-  invert_selection = "inverse",
-  invert_tabline = "",
-  italic_comments = "italic",
-  italic_strings = "NONE",
+    italic = "italic",
+    bold = "bold",
+    underline = "underline",
+    inverse = "inverse",
+    undercurl = "undercurl",
+    invert_signs = "",
+    invert_selection = "inverse",
+    invert_tabline = "",
+    italic_comments = "italic",
+    italic_strings = "NONE",
 }
 
 -- setting default values
 for k, val in pairs(settings) do
-  local key = "gruvbox_" .. k
-  if vim.g[key] == nil then
-    vim.g[key] = val
-  end
+    local key = "gruvbox_" .. k
+    if vim.g[key] == nil then
+        vim.g[key] = val
+    end
 end
 
 -- styles check
 if not utils.tobool(vim.g.gruvbox_bold) then
-  styles.bold = "NONE"
+    styles.bold = "NONE"
 end
 
 if not utils.tobool(vim.g.gruvbox_underline) then
-  styles.underline = "NONE"
+    styles.underline = "NONE"
 end
 
 if not utils.tobool(vim.g.gruvbox_italic) then
-  styles.italic = "NONE"
+    styles.italic = "NONE"
 end
 
 if not utils.tobool(vim.g.gruvbox_inverse) then
-  styles.inverse = "NONE"
+    styles.inverse = "NONE"
 end
 
 if not utils.tobool(vim.g.gruvbox_undercurl) then
-  styles.undercurl = "NONE"
+    styles.undercurl = "NONE"
 end
 
 if utils.tobool(vim.g.gruvbox_invert_signs) then
-  styles.invert_signs = "inverse"
+    styles.invert_signs = "inverse"
 end
 
 if not utils.tobool(vim.g.gruvbox_invert_selection) then
-  styles.invert_selection = "NONE"
+    styles.invert_selection = "NONE"
 end
 
 if utils.tobool(vim.g.gruvbox_invert_tabline) then
-  styles.invert_tabline = "inverse"
+    styles.invert_tabline = "inverse"
 end
 
 if not utils.tobool(vim.g.gruvbox_italicize_comments) then
-  styles.italic_comments = "NONE"
+    styles.italic_comments = "NONE"
 end
 
 if utils.tobool(vim.g.gruvbox_italicize_strings) then
-  styles.italic_strings = "italic"
+    styles.italic_strings = "italic"
 end
 
-return {settings = settings, styles = styles}
+return { settings = settings, styles = styles }
